620036c25602a929ff526888402d6f9cbdad7eb2
ValentinDutin <valentin.dutin@gmail.com>
2025-03-05 00:37:16 +0300

    Added FileLogger

---
+++ CardsServer.Api.FileLogger/FileLoggerProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CardsServer.Api.FileLogger
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private string _filePath;
+        public FileLoggerProvider(string filePath)
+        {
+            _filePath = filePath;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(_filePath);
+        }
+        public void Dispose() { }
+    }
+}
--- CardsServer.Api/Program.cs
+++ CardsServer.Api/Program.cs
@@ -1,10 +1,10 @@
 using MediatR;
-using CommonFiles.Models;
 using CommonFiles.Services;
-using Microsoft.Extensions.DependencyInjection;
 using CardsServer.Data;
 using System.Reflection;
 using CardsServer.Api.Behaviors;
+using CardsServer.Api.FileLogger;
+//using Microsoft.Extensions.Logging.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +13,9 @@ var assemblies = Assembly.Load("CardsServer.Application");
 builder.Services.AddControllers();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
 builder.Services.AddSingleton<IConfigDataService, ConfigDataService>();
+builder.Logging.AddProvider(new FileLoggerProvider("log.txt"));
 builder.Services.AddSingleton<CardsRepository>();
 builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

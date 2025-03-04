b13e0b5f9065d344784307e6b697ed1713babaa2
ValentinDutin <valentin.dutin@gmail.com>
2025-03-05 00:44:53 +0300

    added a few minor changes

--- CardsServer.Api.FileLogger/FileLogger.cs
+++ CardsServer.Api.FileLogger/FileLogger.cs
@@ -14,15 +14,12 @@ namespace CardsServer.Api.FileLogger
         {
             return this;
         }
-
         public void Dispose() { }
-
         public bool IsEnabled(LogLevel logLevel)
         {
             //return logLevel == LogLevel.Trace;
             return true;
         }
-
         public void Log<TState>(LogLevel logLevel, EventId eventId,
                     TState state, Exception? exception, Func<TState, Exception?, string> formatter)
         {
